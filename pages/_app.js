41185a5349c9f2d99de7609974ee6904ed946f39
David Mbadinga <34944852+DubaniWarrior@users.noreply.github.com>
2021-10-05 06:53:53 +0100

    updated gtag

--- pages/_app.js
+++ pages/_app.js
@@ -1,44 +1,70 @@
-import { ApolloProvider } from "@apollo/client";
+import { useEffect } from "react";
 import { useApollo } from "../apollo/client";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Layout from "../containers/Layout";
-import withData from "../lib/withData";
+import * as gtag from "../lib/gtag";
+import Script from "next/script";
+import { useRouter } from "next/router";
 
-import NProgress from 'nprogress';
+import NProgress from "nprogress";
 import Router from "next/router";
 //la barre de chargement
-Router.events.on('routeChangeStart',()=>NProgress.start());
-Router.events.on('routeChangeComplete',()=>NProgress.done());
-Router.events.on('routeChangeError',()=>NProgress.done());
-
-
+Router.events.on("routeChangeStart", () => NProgress.start());
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
 
 //TODO:Swap with our own
-import 'nprogress/nprogress.css';
-import '../public/styles/nprogress.css';
+import "nprogress/nprogress.css";
+import "../public/styles/nprogress.css";
 
- function App({ Component, pageProps,apollo}) {
-  // const apolloClient = useApollo(pageProps.initialApolloState);
-  //console.log(apollo);
+function App({ Component, pageProps, apollo }) {
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
-    <ApolloProvider client={apollo}>
+    <>
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+      />
+      <Script
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
+
       <Layout>
         <Component {...pageProps} />
       </Layout>
-    </ApolloProvider>
+    </>
   );
 }
 
-App.getInitialProps=async function ({Component,ctx}){
- 
-  let pageProps={};
-  if(Component.getInitialProps){
-    pageProps=await Component.getInitialProps(ctx);
+App.getInitialProps = async function ({ Component, ctx }) {
+  let pageProps = {};
+  if (Component.getInitialProps) {
+    pageProps = await Component.getInitialProps(ctx);
   }
 
-  pageProps.query=ctx.query;
+  pageProps.query = ctx.query;
 
-  return {pageProps};
-
-}
-export default withData(App);
+  return { pageProps };
+};
+export default App;
