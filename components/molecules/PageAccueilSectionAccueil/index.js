b19441cd48dacb7688876f6f78c71d1e8d6a961f
David Mbadinga <34944852+DubaniWarrior@users.noreply.github.com>
2021-10-17 10:20:53 +0100

    updated

--- components/molecules/PageAccueilSectionAccueil/index.js
+++ components/molecules/PageAccueilSectionAccueil/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect} from "react";
+import React, { useEffect } from "react";
 import { Container, Row, Col } from "reactstrap";
 import Bouton from "../../shared/Bouton";
 import Texte from "../../shared/Texte";
@@ -30,18 +30,18 @@ function PageAccueilSectionAccueil() {
       ease: "slow(0.7, 0.7, false)",
       x: 100,
     })
-    .from(".PageAccueilSectionAccuel .right", {
-      delay:0,
-      x: 100,
-      duration: 1,
-      opacity: 0,
-      ease: "slow(0.7, 0.7, false)",
-    })
-    .from(".PageAccueilSectionAccuel .pageTitle", {
-      y: 100,
-      duration: 0.2,
-      opacity: 0,
-    })
+      .from(".PageAccueilSectionAccuel .right", {
+        delay: 0,
+        x: 100,
+        duration: 1,
+        opacity: 0,
+        ease: "slow(0.7, 0.7, false)",
+      })
+      .from(".PageAccueilSectionAccuel .pageTitle", {
+        y: 100,
+        duration: 0.2,
+        opacity: 0,
+      })
 
       .from(".PageAccueilSectionAccuel .MyTitle", {
         duration: 0.2,
@@ -65,24 +65,23 @@ function PageAccueilSectionAccueil() {
 
   return (
     <PageAccueilSectionAccueilStc className="PageAccueilSectionAccuel">
-       <DubaniOverlay width="70%" />
+      <DubaniOverlay width="70%" />
       <Container>
-        <Row className="" style={{position:"relative",zIndex:"2"}}>
+        <Row className="" style={{ position: "relative", zIndex: "2" }}>
           <Col lg={7} className=" align-self-center ">
-           
             <div
               className="pr-0 pr-md-5 mr-0 mr-md-5"
               style={{ position: "relative", zIndex: 1 }}
             >
               <Title className="pageTitle py-2 py-md-3">Dubani Agency</Title>
               <Title niveau={1} className="MyTitle">
-                Créer un site web personnalisée pour générer plus de trafic
+                Créer un site web personnalisée pour générer plus de trafic,
               </Title>
               <Texte className="PageText h5 py-2 py-md-3 ">
                 Avec une agence experte dans la création de site web
               </Texte>
               <Bouton className="btn" href="/contact">
-                  Embauchez nous
+                Embauchez nous
               </Bouton>
             </div>
           </Col>
@@ -94,9 +93,7 @@ function PageAccueilSectionAccueil() {
           </Col>
         </Row>
       </Container>
-      <div className="cover1 d-none d-lg-block">
-
-      </div>
+      <div className="cover1 d-none d-lg-block"></div>
     </PageAccueilSectionAccueilStc>
   );
 }
